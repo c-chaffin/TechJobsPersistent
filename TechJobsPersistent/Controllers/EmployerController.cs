f9666e35a5a6999a74a7054d856e9b6fa1c2f189
Christopher Chaffin <christopher.chaffin@gmail.com>
2020-12-30 12:50:46 -0600

    Updated EmployerController

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
+using TechJobsPersistent.Data;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,25 +13,54 @@ namespace TechJobsPersistent.Controllers
 {
     public class EmployerController : Controller
     {
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext)
+        {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            return View();
+            List<Employer> employers = context.Employers.ToList();
+
+            return View(employers);
         }
 
+        [HttpGet]
         public IActionResult Add()
         {
-            return View();
+            AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
+
+            return View(addEmployerViewModel);
         }
 
-        public IActionResult ProcessAddEmployerForm()
+        [HttpPost]
+        public IActionResult ProcessAddEmployerForm(AddEmployerViewModel addEmployerViewModel)
         {
-            return View();
+            if (ModelState.IsValid)
+            {
+                Employer newEmployer = new Employer
+                {
+                    Name = addEmployerViewModel.Name,
+                    Location = addEmployerViewModel.Location
+                };
+
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+
+                return Redirect("/employer");
+            }
+
+            return View("Add", addEmployerViewModel);
         }
 
         public IActionResult About(int id)
         {
-            return View();
+            Employer theEmployer = context.Employers.Find(id);
+
+            return View(theEmployer);
         }
     }
 }
