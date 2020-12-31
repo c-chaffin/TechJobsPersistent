5d192a04b62a214da20d355a49937b8a0cc9b5fd
Christopher Chaffin <christopher.chaffin@gmail.com>
2020-12-31 14:24:54 -0600

    Completed Assignment 4

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,9 +1,14 @@
 --Part 1
-Id int
-Name VARCHAR
-EmployerId int
+
+
 
 --Part 2
+SELECT Name
+From Employers
+Where Location = "St. Louis";
 
 --Part 3
-
+SELECT Name, Description
+FROM skills
+Left JOIN jobskills on skills.Id = jobskills.SkillId where jobskills.JobId IS NOT NULL
+order by Name ASC;
